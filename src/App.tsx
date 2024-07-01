9e003958e5f0c12e9945edd3a37c58857b9a780e
ArrayToFuture <Mochalovdy@yandex.ru>
2024-07-02 00:07:16 +0300

    f*ck

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Button from './components/Button';
+import Button from './components/Button.tsx';
+import Header from './components/Header.tsx';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,31 @@
-import  React from 'react'
+import React from 'react';
+import './Header.css'; // если вы планируете использовать стили
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <img></img>
+      </div>
+      <div>
+        <ul>
+          <li>О нас</li>
+          <li>Возможности</li>
+          <li>Партнёры</li>
+          <li>Контакты</li>
+        </ul>
+      </div>
+      <div>
+      <button>
+        Вход
+      </button>
+      <button>
+        Регистрация
+      </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
